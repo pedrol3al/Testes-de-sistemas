3085efce1823d3077e9ec59039e7fa684d3d5fce
Pedro Gabriel Leal Costa <pedrogabrieleal@gmail.com>
2025-07-29 14:34:46 -0300

    Testes com login

---
+++ teste_login.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("C:/Users/pedro_g_costa/Documents/teste_de_sistemas/login.html")
+
+time.sleep(1)
+time.sleep(8)
+
+ # Preenche o campo de usuário
+usuario_input = driver.find_element(By.ID, "username")
+usuario_input.send_keys("admin")
+time.sleep(1)
+
+ # Preenche o campo de senha
+senha_input = driver.find_element(By.ID, "password")
+senha_input.send_keys("123456")
+
+ # Clica no botão de login
+botao_login = driver.find_element(By.CSS_SELECTOR, "button [type='submit']")
+botao_login.click()
+
+if "Cadastro de Cliente" in driver.page_source:
+    print("Login realizado com sucesso e redirecionado para index.html!")
+else:
+ print("Falha no login ou redirecionamento.")
+
+print("Título atual da página:", driver.title)
+
+# Encerra o navegador
+ #driver.quit()
